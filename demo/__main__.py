d556004d9458ee0e35287f28afb3460cce8e94a6
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-10-23 22:15:46 -0700

    Use Hovercraft in main

--- demo/__main__.py
+++ demo/__main__.py
@@ -1,5 +1,17 @@
-from demo.utils import uppercase
+from demo.utils import InvalidHovercraftContents, Hovercraft, uppercase
 
 
 def main():
     print(uppercase('Hello, World!'))
+
+    try:
+        hovercraft = Hovercraft()
+        hovercraft.contents = 'dogs'
+    except InvalidHovercraftContents as err:
+        print(err)
+
+    try:
+        hovercraft = Hovercraft()
+        hovercraft.contents = 'eels'
+    except InvalidHovercraftContents as err:
+        print(err)
